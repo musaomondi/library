c2fa30325905a303cb0fd2121c3fe995c5780e19
musaomondi <amokem@yahoo.com>
2016-07-05 12:58:37 +0300

    generate user controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'help' => 'static_pages#help'
   get 'about'=> 'static_pages#about'
   get 'contact' => 'static_pages#contact'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :users
 end
