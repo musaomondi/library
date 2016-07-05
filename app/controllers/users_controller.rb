ad1127d7732901c88d3ddef46c8dc4aa4b5cdca5
musaomondi <amokem@yahoo.com>
2016-07-05 14:42:08 +0300

    saving user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,28 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def index
   end
 
   def edit
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
