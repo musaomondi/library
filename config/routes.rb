a6b70f7052a4ff7990d9290a81893234b3ca22aa
musaomondi <amokem@yahoo.com>
2016-07-07 11:46:54 +0300

    solving route generation error

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   get 'help' => 'static_pages#help'
   get 'about'=> 'static_pages#about'
   get 'contact' => 'static_pages#contact'
-  get 'login' => 'sessions/new'
+  get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
+  get 'show' => 'users#show'
   
   resources :users
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,23 +1,8 @@
 require 'test_helper'
 
-class UsersControllerTest < ActionDispatch::IntegrationTest
+class UsersControllerTest < ActionController::TestCase
   test "should get new" do
-    get users_new_url
-    assert_response :success
-  end
-
-  test "should get show" do
-    get users_show_url
-    assert_response :success
-  end
-
-  test "should get index" do
-    get users_index_url
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get users_edit_url
+    get :new
     assert_response :success
   end
 
