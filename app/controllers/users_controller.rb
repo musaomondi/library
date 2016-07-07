0073ee9fa43887d641e7830816a4f35c4de9deb0
musaomondi <amokem@yahoo.com>
2016-07-07 19:46:19 +0300

    user log in test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Welcome to the Maseno University Library!"
       redirect_to @user
     else
       render 'new'
