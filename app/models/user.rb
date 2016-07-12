a8dd3ab8cdb0ddd6638b783c0fbaa4c47405a8f7
musaomondi <amokem@yahoo.com>
2016-07-12 11:29:35 +0300

    adding account activation code to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  before_save { email.downcase! }
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -11,7 +12,7 @@ class User < ApplicationRecord
 
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : 
-                                                  BCrypt::Engine.cost
+      BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
@@ -30,6 +31,15 @@ class User < ApplicationRecord
   end
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  private
+  def downcase_email
+    self.email = email.downcase
+  end
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 
 end
