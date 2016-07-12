d2734e82276bb2a8f97e6b5afc93ff3a39e96b6c
musaomondi <amokem@yahoo.com>
2016-07-12 08:26:38 +0300

    logging out from persistent session

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,20 +5,36 @@ module SessionsHelper
   end
 
   def remember(user)
-    cookies[:remember_token] = { value: remember_token, expires: 20.years.from_now.utc }
+    cookies[:remember_token] = { value: remember_token, expires: 7.days.from_now.utc }
     cookies.signed[:user_id]=user.id
   end
 
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   def logged_in?
     !current_user.nil?
   end
 
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   def log_out
+    forget(current_user)
     session.delete(:user_id)
+    @current_user = nil
   end
 
 end
