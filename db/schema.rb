3949937df48d69ec79bd3805b058ecf967d65f25
musaomondi <amokem@yahoo.com>
2016-07-13 14:09:39 +0300

    define view templates for plain text email and HTML email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160712081749) do
+ActiveRecord::Schema.define(version: 20160713062414) do
 
   create_table "books", force: :cascade do |t|
     t.string   "category"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20160712081749) do
     t.string   "activation_digest"
     t.boolean  "activated"
     t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
