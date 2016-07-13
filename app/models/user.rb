ea6234a0ad42e4cafa4121e2ba0d2b174eaf9ead
musaomondi <amokem@yahoo.com>
2016-07-13 15:30:14 +0300

    add a method defining expiration time of password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -48,7 +48,11 @@ class User < ApplicationRecord
     update_attribute(:reset_sent_at, Time.zone.now)
   end
   def send_password_reset_email
-    UserMailer.password_reset(self).deliver-now
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
   end
 
   private
