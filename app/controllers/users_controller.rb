2d6ade7f39fd02587718a3204daea6b82ed15a4c
musaomondi <amokem@yahoo.com>
2016-07-14 16:49:53 +0300

    migration to add a boolean admin attribute to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
   before_action :correct_user, only: [:edit, :update]
+  
   def new
     @user = User.new
   end
@@ -47,6 +48,6 @@ class UsersController < ApplicationController
   end
   def correct_user
     @user = User.find_by(id: params[:id])
-    redirect_to(root_url) unless @user == current_user?(@user)
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
