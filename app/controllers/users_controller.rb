87eeba9ac29b20a9f000448be88be12b52cdc108
musaomondi <amokem@yahoo.com>
2016-07-14 08:25:14 +0300

    adding update method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,13 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
   end
 
+  def update
+    @user = User.find_by(id: params[:id])
+    if @user.update_attributes(user_params)
+    else
+      render 'edit'
+    end
+  end
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
