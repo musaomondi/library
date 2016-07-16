3a560eed9193d7092b304cf956f8d1d5ccde5121
musaomondi <amokem@yahoo.com>
2016-07-16 22:19:31 +0300

    adding a working destroy action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
   
   def new
@@ -34,6 +34,11 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
   end
   private
   def user_params
