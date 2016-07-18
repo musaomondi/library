cdd5a41490325e17759502b075222a325470bf7c
musaomondi <amokem@yahoo.com>
2016-07-18 16:57:38 +0300

    adding before filter to restrict users not to perform certain actions unless admin

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+ before_action :logged_in_user, only: [:show, :index] 
+ before_action :admin, only:  [:create, :new, :edit, :update, :destroy]
   def new
     @book = Book.new
   end
@@ -15,13 +17,13 @@ class BooksController < ApplicationController
     @books = Book.all
   end
   def show
+    @book = Book.find(params[:id])
   end
   def edit
-    @book=Book.find_by(params[:id])
+    @book = Book.find(params[:id])
   end
   def update
-    @book =Book.find(params[:id])
-
+    @book = Book.find(params[:id])
     if @book.update(book_params)
       redirect_to books_path
     else
@@ -29,7 +31,7 @@ class BooksController < ApplicationController
     end
   end
   def destroy
-    @book=Book.find_by(params[:id])
+    @book=Book.find(params[:id])
     @book.destroy
 
     redirect_to books_path
@@ -38,6 +40,16 @@ class BooksController < ApplicationController
   private
   def book_params
     params.require(:book).permit(:category, :title, :edition, :author, :year_published)
+  end
+  def admin
+    redirect_to(books_path) unless current_user.admin?
+  end
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "You can't access this."
+      redirect_to login_path
+    end
   end
 end
 
