ea42ecc48e9434672017c77f1f45cfa201de926c
musaomondi <amokem@yahoo.com>
2016-07-18 19:56:40 +0300

    styling home page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160714135153) do
+ActiveRecord::Schema.define(version: 20160718125649) do
 
   create_table "books", force: :cascade do |t|
     t.string   "category"
@@ -20,7 +20,9 @@ ActiveRecord::Schema.define(version: 20160714135153) do
     t.integer  "year_published"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.integer  "onloan"
   end
+
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
