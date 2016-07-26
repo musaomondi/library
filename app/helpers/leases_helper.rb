bbf35d6dc337092cfcb5e579a03be6e9c749774b
musaomondi <amokem@yahoo.com>
2016-07-26 18:02:46 +0300

    define method for remaining days

--- app/helpers/leases_helper.rb
+++ app/helpers/leases_helper.rb
@@ -1,2 +1,6 @@
 module LeasesHelper
+  def remaining_days(lease)
+    distance_of_time_in_words(lease.accept.created_at + 14.days, lease.accept.created_at)
+    # (lease.created_at + 14.days).to_date
+  end
 end
