6372bdaf07a4dc5348db5df3b48f925f4e201941
musaomondi <amokem@yahoo.com>
2016-08-18 20:03:19 +0300

    adding images and styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160725123738) do
+ActiveRecord::Schema.define(version: 20160818153945) do
 
   create_table "books", force: :cascade do |t|
     t.string   "category"
@@ -34,6 +34,15 @@ ActiveRecord::Schema.define(version: 20160725123738) do
     t.index ["book_id"], name: "index_leases_on_book_id"
     t.index ["user_id", "book_id"], name: "index_leases_on_user_id_and_book_id", unique: true
     t.index ["user_id"], name: "index_leases_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
