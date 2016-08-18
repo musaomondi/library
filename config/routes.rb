cbb4ea9a9326ac7f8f68926e8b663b4c241fb805
musaomondi <amokem@yahoo.com>
2016-08-18 19:18:18 +0300

    adding messages resources to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
   get 'accept' => 'leases#accept'
+  get 'activate' => 'users#activate'
+  resources :messages, only: [:create, :index]
   resources :users
   resources :books
   resources :leases
