f6a2c5b9b4d4421da1b9f00702c6144a112ff043
musaomondi <amokem@yahoo.com>
2016-08-21 15:12:49 +0300

    making sure a user activates account account before logging in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,6 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       UserMailer.account_activation(@user).deliver_now
-      # log_in @user
       flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
     else
