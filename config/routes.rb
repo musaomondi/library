0e9a55da190aaf410dc210d2bd88f507776198f8
musaomondi <amokem@yahoo.com>
2020-08-26 22:03:29 +0300

    update gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,3 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get 'help' => 'static_pages#help'
-  get 'about'=> 'static_pages#about'
-  get 'contact' => 'static_pages#contact'
-  get 'login' => 'sessions#new'
-  post 'login' => 'sessions#create'
-  delete 'logout' => 'sessions#destroy'
-  get 'accept' => 'leases#accept'
-  get 'activate' => 'users#activate'
-  resources :messages, only: [:create, :index]
-  resources :users
-  resources :books
-  resources :leases
-  resources :account_activations, only: [:edit]
-  resources :password_resets, only: [:new, :create, :edit, :update]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
-
